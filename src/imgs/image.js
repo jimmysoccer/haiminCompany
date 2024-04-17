fc629f3af9b88454634b09f52b4bb11e1b731090
jimmysoccer <jimmysoccer0927@gmail.com>
2024-04-17 14:44:25 -0400

    add portal page

---
+++ src/imgs/image.js
@@ -0,0 +1,5 @@
+import computerScreen from "./computer-screen.svg";
+import intranet from "./intranet-technical.svg";
+
+export const computerScreenIcon = computerScreen;
+export const intranetIcon = intranet;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { Rating, Typography } from "@mui/material";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import "./index.css";
 
@@ -6,7 +7,7 @@ function Home() {
   const [rate, setRate] = useState(0);
   return (
     <>
-      <div className="index">
+      <div className="index text-center ">
         <text>海运公司Template</text>
       </div>
       <Typography component="legend">Controlled</Typography>
