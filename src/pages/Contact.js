af2456e1b23dcce9acff8e1b74549544b6c7fc8e
jimmysoccer <jimmysoccer0927@gmail.com>
2024-04-27 15:06:32 -0400

    add contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,25 @@
+import { Grid } from "@mui/material";
+import { workingIcon } from "../imgs/image";
+
 export default function Contact() {
-  return <div className="container text-center">Contact US</div>;
+  return (
+    <div className="container text-center">
+      <h2 className="my-5">联系我们</h2>
+      <Grid container justifyContent={"center"}>
+        <Grid item xs={12} md={10}>
+          <div>
+            <img src={workingIcon} alt="working" className="img-fluid"></img>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className="my-5">
+          <h3>地址</h3>
+          <p>上海市</p>
+          <h3>联系电话</h3>
+          <p>XXXXXXX</p>
+          <h3>邮箱</h3>
+          <p>XXXXX</p>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
