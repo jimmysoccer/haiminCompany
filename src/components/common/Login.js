aff447833b9e6c1a681c13145377de5f332ce02c
jimmysoccer <jimmysoccer0927@gmail.com>
2024-08-01 22:24:07 -0400

    add login api

--- src/components/common/Login.js
+++ src/components/common/Login.js
@@ -5,16 +5,32 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { Grid } from "@mui/material";
 import { getLoginAuth } from "../../services/get-login-auth";
+import { useAtom } from "jotai";
+import { accessRoleAtom } from "../../atoms/atom";
+import { EDITOR, VISITOR } from "../../constants/constant";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [accessRole, setAccessRole] = useAtom(accessRoleAtom);
+  const [loginError, setLoginError] = useState(false);
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await getLoginAuth(username, password);
-    console.log("res", res);
-    window.location.href = "/home";
+    try {
+      const res = await getLoginAuth(username, password);
+      if (res.status === 200) {
+        setAccessRole(EDITOR);
+        window.location.href = "/home";
+      } else {
+        setAccessRole(VISITOR);
+        setLoginError(true);
+      }
+    } catch (error) {
+      console.error("get login auth error", error);
+      setAccessRole(VISITOR);
+      setLoginError(true);
+    }
   };
   return (
     <div className="container">
@@ -22,43 +38,60 @@ const Login = () => {
         <Grid item>
           <Box p={2}>
             <Typography variant="h6" gutterBottom>
-              Login
+              {accessRole === EDITOR ? "您的账户" : "用户登录"}
             </Typography>
-            <form
-              onSubmit={(e) => {
-                handleLogin(e);
-              }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="Username"
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
+            {accessRole === VISITOR && (
+              <form
+                onSubmit={(e) => {
+                  handleLogin(e);
                 }}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                label="Password"
-                type="password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
               >
-                Sign In
-              </Button>
-            </form>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="用户名"
+                  value={username}
+                  error={loginError}
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  label="密码"
+                  type="password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  登录
+                </Button>
+              </form>
+            )}
+            {accessRole === EDITOR && (
+              <div>
+                <h2>您已经成功登录</h2>
+                <Button
+                  variant="contained"
+                  size="large"
+                  onClick={() => {
+                    setAccessRole(VISITOR);
+                  }}
+                >
+                  退出登录
+                </Button>
+              </div>
+            )}
           </Box>
         </Grid>
       </Grid>
--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,1 +1,5 @@
-export const GET_LOGIN_AUTH_URL = "";
+const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/api"
+    : "https://sh-haimin.cn/api";
+export const GET_LOGIN_AUTH_URL = `${API_URL}/login/get_user_auth`;
