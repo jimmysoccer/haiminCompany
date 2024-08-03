d660f56612e224d9b6a088e3e1d0818bd48fe6e7
jimmysoccer <jimmysoccer0927@gmail.com>
2024-08-02 22:05:14 -0400

    modify api

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,7 +1,9 @@
-const API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8000/api"
-    : "https://sh-haimin.cn/api";
+// const API_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:8000/api"
+//     : "https://sh-haimin.cn/api";
+
+const API_URL = "https://sh-haimin.cn/api";
 export const GET_LOGIN_AUTH_URL = `${API_URL}/login/get_user_auth`;
 export const POST_IMAGES_URL = `${API_URL}/projects/upload_images`;
 export const POST_PROJECT_URL = `${API_URL}/projects/upload_project`;
